4fad22858a0894a58f7b219feea5c095ffbebd02
Rishabh <90997234+Rishabh-441@users.noreply.github.com>
2024-10-28 20:34:04 +0530

    https changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const YOUTUBE_API_KEY = "AIzaSyAhiBEGFYF1_GP_xVD1kC9ymQuGFA5mEMs";
 export const YOUTUBE_API_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ YOUTUBE_API_KEY;
-export const YOUTUBE_SEARCH_SUGGESTION_URL = "http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
+export const YOUTUBE_SEARCH_SUGGESTION_URL = "https://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=";
 export const YOUTUBE_COMMENT_API_URL = "https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&key=" + YOUTUBE_API_KEY + "&videoId=";
 export const YOUTUBE_SEARCH_RESULTS_API = "https://www.googleapis.com/youtube/v3/search?key="+ YOUTUBE_API_KEY + "&part=snippet&maxResults=25&type=video&q=";
 export const YOUTUBE_VIDEO_CATEGORY_LIST_API = "https://youtube.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=IN&key="+YOUTUBE_API_KEY;
